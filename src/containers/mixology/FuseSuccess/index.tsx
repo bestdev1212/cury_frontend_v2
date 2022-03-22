745878467e5a68e324928fb2398b72e3dd0b1d32
TrueLifeDev <truelife1228@outlook.com>
2022-03-22 16:46:52 -0500

    minor update

--- src/containers/mixology/FuseSuccess/index.tsx
+++ src/containers/mixology/FuseSuccess/index.tsx
@@ -6,7 +6,7 @@ const FuseSuccess: React.FC = (): JSX.Element => {
     const [appState, setAppState] = useAppContext();
 
     return (
-        <Stack height="100%" justifyContent="flex-end">
+        <Stack>
             <Typography fontSize={48} fontWeight={700} lineHeight={1.1} textAlign="center">
                 You have successfully fused here is your{' '}
                 <Typography fontSize={48} fontWeight={700} lineHeight={1.1} color="#FFCA21">
