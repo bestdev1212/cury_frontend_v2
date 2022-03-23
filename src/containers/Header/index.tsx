f537ed9cad144f29c6d36194ad5cac8e5c587d63
TrueLifeDev <truelife1228@outlook.com>
2022-03-23 13:55:34 -0500

    Wallet connect dlg added

--- src/containers/Header/index.tsx
+++ src/containers/Header/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Stack, Box, Typography, IconButton } from '@mui/material';
+import React, { useState } from 'react';
+import { Stack, Box, Typography, IconButton, Dialog, DialogTitle, DialogContent } from '@mui/material';
 import Container from '../Container';
 import Image from 'next/image';
 import LogoImg from '../../assets/curry-logo.png';
@@ -9,6 +9,8 @@ import { useRouter } from 'next/router';
 import OpenseaIcon from '../../assets/opensea.svg';
 import DiscordIcon from '../../assets/discord.svg';
 import TwitterIcon from '../../assets/twitter.svg';
+import { useWeb3React } from '@web3-react/core';
+import WalletConnectDlg from '../../components/WalletConnectDlg';
 
 type ComponentProps = {};
 
@@ -23,38 +25,64 @@ const menuList = [
 
 const Header: React.FC<ComponentProps> = ({}) => {
     const router = useRouter();
+    const [openConnectWalletDlg, setOpenConnectWalletDlg] = useState(false);
+    const { active, account, library, connector, activate, deactivate } = useWeb3React();
 
     return (
-        <Box sx={{ background: '#1B1C22' }}>
-            <Container>
-                <Stack height={72} direction="row" alignItems="center" justifyContent="space-between">
-                    <Stack direction="row" alignItems="center" spacing={5}>
-                        <Image src={LogoImg} width={40} height={40} alt="Logo" />
-                        <Stack direction="row" spacing={2}>
-                            {menuList.map((item, index) => (
-                                <Link href={item.url} passHref>
-                                    <HeaderMenuBtn selected={router.pathname === item.url}>
-                                        <Typography>{item.title}</Typography>
-                                    </HeaderMenuBtn>
-                                </Link>
-                            ))}
+        <>
+            <Box sx={{ background: '#1B1C22' }}>
+                <Container>
+                    <Stack height={72} direction="row" alignItems="center" justifyContent="space-between">
+                        <Stack direction="row" alignItems="center" spacing={5}>
+                            <Image src={LogoImg} width={40} height={40} alt="Logo" />
+                            <Stack direction="row" spacing={2}>
+                                {menuList.map((item, index) => (
+                                    <Link href={item.url} passHref>
+                                        <HeaderMenuBtn selected={router.pathname === item.url}>
+                                            <Typography>{item.title}</Typography>
+                                        </HeaderMenuBtn>
+                                    </Link>
+                                ))}
+                            </Stack>
+                        </Stack>
+                        <Stack direction="row" alignItems="center" spacing={2}>
+                            <IconButton>
+                                <OpenseaIcon />
+                            </IconButton>
+                            <IconButton>
+                                <DiscordIcon />
+                            </IconButton>
+                            <IconButton>
+                                <TwitterIcon />
+                            </IconButton>
+                            <ConnectWalletBtn
+                                onClick={() => {
+                                    if (!active) setOpenConnectWalletDlg(true);
+                                }}
+                            >
+                                CONNECT WALLET
+                            </ConnectWalletBtn>
                         </Stack>
                     </Stack>
-                    <Stack direction="row" alignItems="center" spacing={2}>
-                        <IconButton>
-                            <OpenseaIcon />
-                        </IconButton>
-                        <IconButton>
-                            <DiscordIcon />
-                        </IconButton>
-                        <IconButton>
-                            <TwitterIcon />
-                        </IconButton>
-                        <ConnectWalletBtn>CONNECT WALLET</ConnectWalletBtn>
-                    </Stack>
-                </Stack>
-            </Container>
-        </Box>
+                </Container>
+            </Box>
+            <Dialog
+                open={openConnectWalletDlg}
+                maxWidth="lg"
+                onClose={() => {
+                    setOpenConnectWalletDlg(false);
+                }}
+            >
+                <DialogTitle>
+                    <Typography fontSize={48} fontWeight={700} color="black">
+                        Connect Your Wallet
+                    </Typography>
+                </DialogTitle>
+                <DialogContent sx={{ width: 640 }}>
+                    <WalletConnectDlg onChange={() => {}} />
+                </DialogContent>
+            </Dialog>
+        </>
     );
 };
 
