4274150e00fb85eb41242bc1dc2bae80d50e22f7
TrueLifeDev <truelife1228@outlook.com>
2022-04-04 11:48:46 -0500

    TheLabs product details page template added

---
+++ src/containers/Lab/ProductDetails/index.tsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+import { Stack, Typography } from '@mui/material';
+import Container from '../../Container';
+
+const ProductDetails: React.FC = (): JSX.Element => {
+    return <Container sx={{ paddingY: 8 }}>ProductDetails</Container>;
+};
+
+export default ProductDetails;
--- src/containers/Lab/index.tsx
+++ src/containers/Lab/index.tsx
@@ -7,6 +7,7 @@ import { useAppContext } from '../../context/AppContext';
 import MutantBox from '../../components/MutantBox';
 import BasketballBox from '../../components/BasketballBox';
 import SerumBox from '../../components/SerumBox';
+import ProductDetails from './ProductDetails';
 
 export enum Categories {
     ALL,
@@ -22,7 +23,7 @@ const LabPageContainer: React.FC = (): JSX.Element => {
     const [appState, setAppState] = useAppContext();
     const [category, setCategory] = useState<Categories>(Categories.ALL);
 
-    const [selectedProductId, setSelectedProductId] = useState<number>();
+    const [selectedProductId, setSelectedProductId] = useState<number>(-1);
 
     const onMutantItemSelect = (id: number) => {
         setSelectedProductId(id);
@@ -37,80 +38,86 @@ const LabPageContainer: React.FC = (): JSX.Element => {
     };
 
     return (
-        <Container sx={{ paddingY: 5, overflow: 'visible' }}>
-            <Stack spacing={5}>
-                <Stack direction="row" spacing={2}>
-                    <CounterBox title="MY BASKETBALLS" value={3} />
-                    <CounterBox title="MY SERUMS" value={9} />
-                    <CounterBox title="MUTANT BASKETBALLS" value={1} />
-                </Stack>
-                <Typography fontSize={48} fontWeight={700} color="white">
-                    The Lab
-                </Typography>
-                <Stack direction="row" spacing={2}>
-                    {categoryButtonsList.map((item, index) => (
-                        <CategoryBtn
-                            key={`category-btn-${index}`}
-                            selected={category === index}
-                            onClick={() => setCategory(index)}
-                        >
-                            {item}
-                        </CategoryBtn>
-                    ))}
-                </Stack>
-                {(category === Categories.ALL || category === Categories.MUTANTS) && (
-                    <Stack spacing={3}>
-                        <Typography fontSize={32} fontWeight={700} color="white">
-                            Mutants
+        <>
+            {selectedProductId === -1 ? (
+                <Container sx={{ paddingY: 5, overflow: 'visible' }}>
+                    <Stack spacing={5}>
+                        <Stack direction="row" spacing={2}>
+                            <CounterBox title="MY BASKETBALLS" value={3} />
+                            <CounterBox title="MY SERUMS" value={9} />
+                            <CounterBox title="MUTANT BASKETBALLS" value={1} />
+                        </Stack>
+                        <Typography fontSize={48} fontWeight={700} color="white">
+                            The Lab
                         </Typography>
-                        <Stack direction="row" spacing={4}>
-                            {appState.mutantsList.map((item) => (
-                                <MutantBox
-                                    item={item}
-                                    selected={selectedProductId === item.id}
-                                    selectable
-                                    onSelect={onMutantItemSelect}
-                                />
+                        <Stack direction="row" spacing={2}>
+                            {categoryButtonsList.map((item, index) => (
+                                <CategoryBtn
+                                    key={`category-btn-${index}`}
+                                    selected={category === index}
+                                    onClick={() => setCategory(index)}
+                                >
+                                    {item}
+                                </CategoryBtn>
                             ))}
                         </Stack>
+                        {(category === Categories.ALL || category === Categories.MUTANTS) && (
+                            <Stack spacing={3}>
+                                <Typography fontSize={32} fontWeight={700} color="white">
+                                    Mutants
+                                </Typography>
+                                <Stack direction="row" spacing={4}>
+                                    {appState.mutantsList.map((item) => (
+                                        <MutantBox
+                                            item={item}
+                                            selected={selectedProductId === item.id}
+                                            selectable
+                                            onSelect={onMutantItemSelect}
+                                        />
+                                    ))}
+                                </Stack>
+                            </Stack>
+                        )}
+                        {(category === Categories.ALL || category === Categories.BASKETBALLS) && (
+                            <Stack spacing={3}>
+                                <Typography fontSize={32} fontWeight={700} color="white">
+                                    Basketballs
+                                </Typography>
+                                <Stack direction="row" spacing={4}>
+                                    {appState.basketballsList.map((item) => (
+                                        <BasketballBox
+                                            item={item}
+                                            selected={selectedProductId === item.id}
+                                            selectable
+                                            onSelect={onBasketballItemSelect}
+                                        />
+                                    ))}
+                                </Stack>
+                            </Stack>
+                        )}
+                        {(category === Categories.ALL || category === Categories.SERUMS) && (
+                            <Stack spacing={3}>
+                                <Typography fontSize={32} fontWeight={700} color="white">
+                                    Serums
+                                </Typography>
+                                <Stack direction="row" flexWrap="wrap" columnGap={4} rowGap={4}>
+                                    {appState.serumsList.map((item) => (
+                                        <SerumBox
+                                            item={item}
+                                            selected={selectedProductId === item.id}
+                                            selectable
+                                            onSelect={onSerumItemSelect}
+                                        />
+                                    ))}
+                                </Stack>
+                            </Stack>
+                        )}
                     </Stack>
-                )}
-                {(category === Categories.ALL || category === Categories.BASKETBALLS) && (
-                    <Stack spacing={3}>
-                        <Typography fontSize={32} fontWeight={700} color="white">
-                            Basketballs
-                        </Typography>
-                        <Stack direction="row" spacing={4}>
-                            {appState.basketballsList.map((item) => (
-                                <BasketballBox
-                                    item={item}
-                                    selected={selectedProductId === item.id}
-                                    selectable
-                                    onSelect={onBasketballItemSelect}
-                                />
-                            ))}
-                        </Stack>
-                    </Stack>
-                )}
-                {(category === Categories.ALL || category === Categories.SERUMS) && (
-                    <Stack spacing={3}>
-                        <Typography fontSize={32} fontWeight={700} color="white">
-                            Serums
-                        </Typography>
-                        <Stack direction="row" flexWrap="wrap" columnGap={4} rowGap={4}>
-                            {appState.serumsList.map((item) => (
-                                <SerumBox
-                                    item={item}
-                                    selected={selectedProductId === item.id}
-                                    selectable
-                                    onSelect={onSerumItemSelect}
-                                />
-                            ))}
-                        </Stack>
-                    </Stack>
-                )}
-            </Stack>
-        </Container>
+                </Container>
+            ) : (
+                <ProductDetails />
+            )}
+        </>
     );
 };
 
