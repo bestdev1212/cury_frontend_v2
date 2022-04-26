eaa122bcbed9dd0a23c78645f94f6b226c23bf03
TrueLifeDev <truelife1228@outlook.com>
2022-04-26 14:42:33 -0500

    fix: update

--- src/web3/connect.ts
+++ src/web3/connect.ts
@@ -3,29 +3,29 @@ import { injected } from './Connector';
 export async function connect(activate: any) {
     try {
         const w: any = window;
-        await w.ethereum.request({
-            method: 'wallet_addEthereumChain',
-            params: [
-                {
-                    chainId: process.env.NEXT_PUBLIC_ENV == 'production' ? '0x89' : '0x4',
-                    chainName: process.env.NEXT_PUBLIC_ENV == 'production' ? 'Polygon' : 'Mumbai Testnet',
-                    nativeCurrency: { name: 'MATIC', symbol: 'MATIC', decimals: 18 },
-                    rpcUrls:
-                        process.env.NEXT_PUBLIC_ENV == 'production'
-                            ? ['https://rpc-mainnet.matic.network']
-                            : ['https://rpc-mumbai.maticvigil.com/'],
-                },
-            ],
-        });
+        // await w.ethereum.request({
+        //     method: 'wallet_addEthereumChain',
+        //     params: [
+        //         {
+        //             chainId: process.env.NEXT_PUBLIC_ENV == 'production' ? '0x1' : '0x4',
+        //             chainName: process.env.NEXT_PUBLIC_ENV == 'production' ? 'Ethereum Mainnet' : 'Ethereum Rinkeby',
+        //             nativeCurrency: { name: 'Ethereum', symbol: 'ETH', decimals: 18 },
+        //             rpcUrls:
+        //                 process.env.NEXT_PUBLIC_ENV == 'production'
+        //                     ? ['https://mainnet.infura.io/v3/']
+        //                     : ['https://rinkeby.infura.io/v3/'],
+        //         },
+        //     ],
+        // });
+
+        // await w.ethereum.request({
+        //     method: 'wallet_switchEthereumChain',
+        //     params: [{ chainId: process.env.NEXT_PUBLIC_ENV == 'production' ? '0x1' : '0x4' }],
+        // });
 
         await w.ethereum.request({
             method: 'wallet_switchEthereumChain',
-            params: [{ chainId: process.env.NEXT_PUBLIC_ENV == 'production' ? '0x89' : '0x4' }],
-        });
-
-        await w.ethereum.request({
-            method: 'wallet_switchEthereumChain',
-            params: [{ chainId: process.env.NEXT_PUBLIC_ENV == 'production' ? '0x89' : '0x4' }],
+            params: [{ chainId: process.env.NEXT_PUBLIC_ENV == 'production' ? '0x1' : '0x4' }],
         });
 
         await activate(injected);
