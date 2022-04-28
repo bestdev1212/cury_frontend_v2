b41c591f8b4b8e46b1667a77b5874894a88b541a
TrueLifeDev <truelife1228@outlook.com>
2022-04-28 16:24:54 -0500

    fix: video update, terms page content update

--- src/containers/FAQ/index.tsx
+++ src/containers/FAQ/index.tsx
@@ -13,87 +13,175 @@ const FAQPageContainer: React.FC = (): JSX.Element => {
                 Got a question? We're here to answer! If you don't see your answer here, contact us through our Discord.
             </Typography>
             <Stack spacing={2} marginTop={6}>
-                <FAQItem title="What is the Curry Counter and how does it work?">
+                <FAQItem title="What is an NFT?">
                     <Typography fontSize={16} fontWeight={400}>
-                        Score to Mint is an interactive fan tool to reward the biggest Stephen Curry fans as he battles
-                        his way through the NBA playoffs. The premise is simple: for every 3-pointer Curry makes in a
-                        playoff game, three free digital basketballs are claimable by the most engaged fans. Score to
-                        Mint kicks off the next iteration of Curry Brand's effort to create the most positive basketball
-                        community of all time, championed by the greatest shooter of all time.<br></br>
-                        <br></br>Only one claim per wallet
+                        NFT are ”non-fungible tokens” — a fancy way of saying it's a unique, one-of-a-kind digital item
+                        that users can buy, own, and trade. Some NFTs' primary functions are digital art and looking
+                        cool; some offer additional utility, like exclusive access to websites or participation in an
+                        event.
                     </Typography>
                 </FAQItem>
-                <FAQItem title="How to mint">
+                <FAQItem title="What is the NF3 Counter?">
                     <Typography fontSize={16} fontWeight={400}>
-                        As each Basketball is an NFT, you'll need a blockchain wallet to get started. Don't worry - this
-                        is as simple as downloading the MetaMask extension in your browser and creating a MetaMask
-                        wallet. Once you have your Metamask wallet, head over to headz.currybrand.com and connect your
-                        wallet. Once your wallet is connected, click “Reserve” to reserve your spot to mint a Basketball
-                        NFT.
+                        NF3 Counter is an interactive fan tool to reward the biggest Stephen Curry fans as he battles
+                        his way through the NBA playoffs. The premise is simple: for every three-pointer, Curry makes in
+                        a playoff game, three free digital basketballs are claimable by the most engaged fans. Score to
+                        Mint kicks off the next iteration of Curry Brand's effort to create the most positive Basketball
+                        community of all time, championed by the greatest shooter of all time.
                     </Typography>
                 </FAQItem>
-                <FAQItem title="What is reserve minting?">
+                <FAQItem title="How does the NF3 Counter work? Step by Step Guide">
                     <Typography fontSize={16} fontWeight={400}>
-                        We chose to build a reserve mechanism to ensure everyone has a fair chance of claiming a
-                        Basketball. Also in the interest of avoiding costly gas wars for users, reserving a Basketball
-                        is a gasless transaction. You simply need to connect your wallet and click “Reserve” faster than
-                        everyone else to claim your spot to reserve a free Basketball.<br></br>
-                        <br></br>If you've successfully reserved a Basketball, you'll receive a success message. Simply
-                        come back to the site at the end of the game to claim your free NFT. Note that if you've
-                        successfully reserved a Basketball, you can claim your NFT at a later date at any time. Claiming
-                        your NFT will incur gas fees, which usually ranges from $7-$25 worth of Ethereum. If you click
-                        “Reserve” but an error message is displayed, someone else beat you to the punch. Come back the
-                        next time Steph Curry scores for the chance to reserve your Basketball again.
+                        For every 3-pointer Curry makes in a playoff game, three free digital basketballs are claimable
+                        by the most engaged fans.
+                        <br></br>
+                        <br></br>
+                        Step 1: Connect your MetaMask Wallet to the site.
+                        <br></br>
+                        Step 2: Go to “Reserve an NF3 Basketball”.
+                        <br></br>
+                        Step 3: When a Basketball is available to Reserve, click “Agree to Terms & Conditions” and then
+                        press “Reserve.”
+                        <br></br>
+                        Step 4: Scroll down to “NF3 Basketball Winners” to check whether you have successfully reserved
+                        a Basketball.
+                        <br></br>
+                        Step 5: If you were successful, please scroll down to “NF3 Counter” and click “Claim” one hour
+                        after the game. Please note: It will take around an hour before you can claim your NF3
+                        Basketball.
                     </Typography>
                 </FAQItem>
-                <FAQItem title="Buying, Selling, Trading, and Refunds">
-                    <Stack spacing={3}>
-                        <Stack spacing={1}>
-                            <Typography fontSize={16} fontWeight={700}>
-                                Can I sell my Basketball?
-                            </Typography>
-                            <Typography fontSize={16} fontWeight={400}>
-                                Yes! You will be able to sell your Basketball on Opensea. We are using Opensea so users
-                                can buy, sell, and trade to and from other users. Please note that when you buy and sell
-                                on these exchanges, you are solely responsible for your Basketball and must abide by the
-                                rules and risks of using the platform.
-                            </Typography>
-                        </Stack>
-                        <Stack spacing={1}>
-                            <Typography fontSize={16} fontWeight={700}>
-                                What if I didn't get a Basketball? Can I buy it somewhere else?
-                            </Typography>
-                            <Typography fontSize={16} fontWeight={400}>
-                                Yes! You will be able to buy Basketball on Opensea off the secondary market from other
-                                users. We are using the Opensea platform so users can buy from other users directly.
-                                Please note that when you buy and sell on these exchanges, you are solely responsible
-                                for your Basketball and must abide by the rules and risks of using the platform. Click
-                                the Opensea icon to be directed to our secondary market collection.
-                            </Typography>
-                        </Stack>
-                        <Stack spacing={1}>
-                            <Typography fontSize={16} fontWeight={700}>
-                                What is Opensea?
-                            </Typography>
-                            <Typography fontSize={16} fontWeight={400}>
-                                Opensea is an NFT marketplace where users can buy, trade, and sell their NFTs. We are
-                                partnering with Opensea to allow our users to safely trade their Basketball. Please
-                                visit opensea.io for more details.
-                            </Typography>
-                        </Stack>
-                        <Stack spacing={1}>
-                            <Typography fontSize={16} fontWeight={700}>
-                                Will I be able to refund my Basketball?
-                            </Typography>
-                            <Typography fontSize={16} fontWeight={400}>
-                                After a successful transaction, you will not be able to refund your Basketball.
-                            </Typography>
-                        </Stack>
-                    </Stack>
+                <FAQItem title="What does “Reserve” do on the NF3 Counter Page?">
+                    <Typography fontSize={16} fontWeight={400}>
+                        The reserve button will activate when Curry scores a three-pointer during the Playoffs. The user
+                        that first clicks “Reserve” will have a free Basketball NFT reserved to claim after the game.
+                        <br></br>
+                        <br></br>
+                        Please note: Even if you successfully reserve a Basketball, it may take some time for the
+                        information to be updated due to the backend system updating all the details.
+                    </Typography>
+                </FAQItem>
+                <FAQItem title="What does Claiming mean?">
+                    <Typography fontSize={16} fontWeight={400}>
+                        Claiming means that you can own the Basketball NFT. When you click “Reserve” and succeed, we are
+                        just reserving your Basketball for you so that you can fully claim it afterward. Users may Claim
+                        the Basketball NFT whenever they like.
+                        <br></br>
+                        <br></br>
+                        Please note: that you will need enough Ethereum in your wallet to pay for the gas fee.
+                        <br></br>
+                        (To pay less gas, track gas prices using https://etherscan.io/gastracker.)
+                        <br></br>
+                        The Gas fee is not something we can control.
+                    </Typography>
+                </FAQItem>
+                <FAQItem title="How many NF3 Basketballs can I claim?">
+                    <Typography fontSize={16} fontWeight={400}>
+                        There is only one Claim per wallet per game.
+                        <br></br>
+                        <br></br>
+                        If you successfully reserve a Basketball NFT, you will not be able to further participate in
+                        reserve until the next game.
+                        <br></br>
+                        If you were unsuccessful in reserving a Basketball NFT, then you can continue participating in
+                        reserving a Basketball until you succeed.
+                        <br></br>
+                        <br></br>
+                        Please note: New game means a new start!
+                    </Typography>
+                </FAQItem>
+                <FAQItem title="What can I do with my NF3 Basketball?">
+                    <Typography fontSize={16} fontWeight={400}>
+                        We will announce more updates as we build the most fanatic basketball community ever! So don’t
+                        miss any updates by following us on Twitter and joining our Discord!
+                    </Typography>
+                </FAQItem>
+                <FAQItem title="What if I wasn't able to get an NF3 Basketball?">
+                    <Typography fontSize={16} fontWeight={400}>
+                        There will be a general mint in the coming future. Follow our Twitter and join Discord to stay
+                        posted.
+                    </Typography>
+                </FAQItem>
+                <FAQItem title="Do I need a digital wallet to get a Basketball?">
+                    <Typography fontSize={16} fontWeight={400}>
+                        Yes, you need a MetaMask wallet to reserve mint an NF3 Basketball! We recommend downloading it
+                        on your computer and adding the extension for ease of reserving.
+                    </Typography>
+                </FAQItem>
+                <FAQItem title="What blockchain is the NFT on?">
+                    <Typography fontSize={16} fontWeight={400}>
+                        It is on the Ethereum blockchain.
+                    </Typography>
+                </FAQItem>
+                <FAQItem title="Where can I get ETH cryptocurrency?">
+                    <Typography fontSize={16} fontWeight={400}>
+                        You can purchase ETH using your native currency on an exchange platform, like Coinbase and
+                        Gemini.
+                    </Typography>
+                </FAQItem>
+                <FAQItem title="What is a gas fee?">
+                    <Typography fontSize={16} fontWeight={400}>
+                        Gas fees are the transaction fees that users pay to have their transactions processed. On the
+                        Ethereum blockchain, gas fees are paid using ETH cryptocurrency. The amount of gas you'll need
+                        to pay to have your transaction processed depends on the number of people trying to process
+                        transactions. There are websites where you can track gas prices, such as
+                        https://etherscan.io/gastracker.
+                    </Typography>
+                </FAQItem>
+                <FAQItem title="Selling, Trading, and Refunds">
+                    <Typography fontSize={16} fontWeight={400}>
+                        Can I sell my NF3 Basketball?
+                        <br></br>
+                        Yes! You will be able to sell your NF3 Basketball on OpenSea. We are using OpenSea so users can
+                        buy, sell, and trade to and from other users. Please note that when you buy and sell on these
+                        exchanges, you are solely responsible for your NF3 Basketball and must abide by the rules and
+                        risks of using the platform.
+                        <br></br>
+                        <br></br>
+                        What if I didn't get an NF3 Basketball? Can I buy it somewhere else?
+                        <br></br>
+                        Yes! You will be able to buy NF3 Basketball on OpenSea off the secondary market from other
+                        users. We are using the OpenSea platform so users can buy from other users directly. Please note
+                        that when you buy and sell on these exchanges, you are solely responsible for your NF3
+                        Basketball and must abide by the rules and risks of using the platform. Click the OpenSea icon
+                        to be directed to our secondary market collection.
+                        <br></br>
+                        <br></br>
+                        What is OpenSea?
+                        <br></br>
+                        OpenSea is an NFT marketplace where users can buy, trade, and sell their NFTs. We partner with
+                        OpenSea to allow our users to trade their NF3 Basketball safely. Please visit opensea.io for
+                        more details.
+                        <br></br>
+                        <br></br>
+                        Will I be able to refund my NF3 Basketball? After a successful transaction, you will not be able
+                        to refund your NF3 Basketball.
+                    </Typography>
+                </FAQItem>
+                <FAQItem title="Where can I view my NFT after I minted it?">
+                    <Typography fontSize={16} fontWeight={400}>
+                        After minting, you can view your NFT in Opensea.
+                    </Typography>
                 </FAQItem>
                 <FAQItem title="What if I'm a Genesis Curry Flow Holder?">
                     <Typography fontSize={16} fontWeight={400}>
                         Please wait for future updates. We have not forgotten about our GCF holders.
+                    </Typography>
+                </FAQItem>
+                <FAQItem title="What happens if I have a Partner Shoe (Decentraland, Gala Games, Rumble Kong League, and The Sandbox)?">
+                    <Typography fontSize={16} fontWeight={400}>
+                        Please wait for future updates. We are working hard with our partners to award holders.
+                    </Typography>
+                </FAQItem>
+                <FAQItem title="As an Under Armour employee, can I use my UA Employee discount on this purchase?">
+                    <Typography fontSize={16} fontWeight={400}>
+                        Unfortunately, UA Employee discount is not available on the purchase of the NF3 Basketball.
+                    </Typography>
+                </FAQItem>
+                <FAQItem title="How can I get Support?">
+                    <Typography fontSize={16} fontWeight={400}>
+                        You can join our Discord and open up a Ticket or email us at contact@lunamarket.io! Also, follow
+                        our Twitter for real-time updates and announcements.
                     </Typography>
                 </FAQItem>
             </Stack>
