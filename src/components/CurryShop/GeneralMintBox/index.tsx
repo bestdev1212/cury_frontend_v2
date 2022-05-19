c8e506a0249a9551419b1943d0554e14817a4671
TrueLifeDev <truelife1228@outlook.com>
2022-05-19 01:09:35 -0500

    feat: general mint box status updated

--- src/components/CurryShop/GeneralMintBox/index.tsx
+++ src/components/CurryShop/GeneralMintBox/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Stack, Typography } from '@mui/material';
+import { Stack, Typography, Dialog, CircularProgress } from '@mui/material';
 import { useWeb3React } from '@web3-react/core';
 import BasketballHeadABI from '../../../lib/ABI/BasketBallHead.json';
 import Image from 'next/image';
@@ -12,11 +12,28 @@ type ComponentProps = {
 
 const MAX_VAL = 3;
 
+enum MintStatus {
+    NOT_MINTED,
+    MINTING,
+    MINT_FAILED,
+    MINT_SUCCESS,
+}
+
+enum ReserveStatus {
+    NOT_RESERVED,
+    RESERVING,
+    RESERVE_FAILED,
+    RESERVE_SUCCESS,
+}
+
 const GeneralMintBox: React.FC<ComponentProps> = ({ amountLeft, disabled = false }): JSX.Element => {
     const { active, account, library, activate } = useWeb3React();
     const [mintAmount, setMintAmount] = useState<string>('');
     const [mintPrice, setMintPrice] = useState<number>(0);
     const [reservedAmount, setReservedAmount] = useState<string>('');
+
+    const [mintState, setMintState] = useState<MintStatus>(MintStatus.NOT_MINTED);
+    const [reserveState, setReserveState] = useState<ReserveStatus>(ReserveStatus.NOT_RESERVED);
 
     const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const value = event.target.value;
@@ -25,6 +42,8 @@ const GeneralMintBox: React.FC<ComponentProps> = ({ amountLeft, disabled = false
 
     const mint = async () => {
         if (!account) return;
+
+        setMintState(MintStatus.MINTING);
 
         const nftContract = new library.eth.Contract(
             BasketballHeadABI,
@@ -44,7 +63,9 @@ const GeneralMintBox: React.FC<ComponentProps> = ({ amountLeft, disabled = false
                     .mint(mintAmount, [])
                     .send({ from: account, value: mintPrice * parseInt(mintAmount) });
             }
+            setMintState(MintStatus.MINT_SUCCESS);
         } catch (err: any) {
+            setMintState(MintStatus.MINT_FAILED);
             console.error(err);
             return;
         }
@@ -52,6 +73,8 @@ const GeneralMintBox: React.FC<ComponentProps> = ({ amountLeft, disabled = false
 
     const reserve = async () => {
         if (!account) return;
+
+        setReserveState(ReserveStatus.RESERVING);
 
         const nftContract = new library.eth.Contract(
             BasketballHeadABI,
@@ -61,15 +84,14 @@ const GeneralMintBox: React.FC<ComponentProps> = ({ amountLeft, disabled = false
         );
 
         try {
-            let dropPhase = await nftContract.methods.dropPhase().call({ from: account });
-            if (parseInt(dropPhase) == 3) {
-                await nftContract.methods
-                    .reserve(mintAmount)
-                    .send({ from: account, value: mintPrice * parseInt(mintAmount) });
-                const reservedCount = await nftContract.methods.reserveCount(account).call({ from: account });
-                setReservedAmount(reservedCount);
-            }
+            await nftContract.methods
+                .reserve(mintAmount)
+                .send({ from: account, value: mintPrice * parseInt(mintAmount) });
+            const reservedCount = await nftContract.methods.reserveCount(account).call({ from: account });
+            setReservedAmount(reservedCount);
+            setReserveState(ReserveStatus.RESERVE_SUCCESS);
         } catch (err: any) {
+            setReserveState(ReserveStatus.RESERVE_FAILED);
             console.error(err);
             return;
         }
@@ -96,63 +118,77 @@ const GeneralMintBox: React.FC<ComponentProps> = ({ amountLeft, disabled = false
     }, [account]);
 
     return (
-        <Stack padding={4} borderRadius={2} sx={{ background: '#1B1C22' }}>
-            <Typography fontSize={48} fontWeight={700} lineHeight={1.1}>
-                General Mint
-            </Typography>
-            <Typography fontWeight={700} marginTop={2}>
-                Free NF3 Basketball Mint for every GCF NFT Holders
-            </Typography>
-            <Stack direction={{ xs: 'column', md: 'row' }} spacing={2} marginTop={3}>
-                <Image
-                    src={'/assets/currycounter/curry-brand.png'}
-                    layout="fixed"
-                    width={160}
-                    height={160}
-                    style={{ borderRadius: 16 }}
-                />
-                <Stack>
-                    <Typography fontWeight={700}>How it works:</Typography>
-                    <Typography marginTop={2}>° A snapshot will be taken on XXX, 2022 at 5PM PST</Typography>
-                    <Typography>
-                        ° On XXX, 2022 at 5PM PST, you may mint an NF3 Basketball for every GCF you hold
-                    </Typography>
-                    <Typography>
-                        ° Please Note: You will need enough Ethereum in your wallet to pay for the gas fee.
-                    </Typography>
+        <>
+            <Stack padding={4} borderRadius={2} sx={{ background: '#1B1C22' }}>
+                <Typography fontSize={48} fontWeight={700} lineHeight={1.1}>
+                    General Mint
+                </Typography>
+                <Typography fontWeight={700} marginTop={2}>
+                    Free NF3 Basketball Mint for every GCF NFT Holders
+                </Typography>
+                <Stack direction={{ xs: 'column', md: 'row' }} spacing={2} marginTop={3}>
+                    <Image
+                        src={'/assets/currycounter/curry-brand.png'}
+                        layout="fixed"
+                        width={160}
+                        height={160}
+                        style={{ borderRadius: 16 }}
+                    />
+                    <Stack>
+                        <Typography fontWeight={700}>How it works:</Typography>
+                        <Typography marginTop={2}>° A snapshot will be taken on XXX, 2022 at 5PM PST</Typography>
+                        <Typography>
+                            ° On XXX, 2022 at 5PM PST, you may mint an NF3 Basketball for every GCF you hold
+                        </Typography>
+                        <Typography>
+                            ° Please Note: You will need enough Ethereum in your wallet to pay for the gas fee.
+                        </Typography>
+                    </Stack>
                 </Stack>
-            </Stack>
-            <Typography fontSize={32} fontWeight={700} marginTop={4}>
-                PRICE: 0.07{' '}
-                <Typography fontWeight={700} display="inline">
-                    (+GAS FEE)
+                <Typography fontSize={32} fontWeight={700} marginTop={4}>
+                    PRICE: 0.07{' '}
+                    <Typography fontWeight={700} display="inline">
+                        (+GAS FEE)
+                    </Typography>
                 </Typography>
-            </Typography>
-            <Stack spacing={1} marginTop={3}>
-                <Typography fontSize={14} fontWeight={400} color="white">
-                    # of Basketball Heads (Max 3)
-                </Typography>
-                <AmountInputWrapper sx={{ width: 184 }}>
-                    <AmountInputTextField value={mintAmount} onChange={handleInputChange} />
-                    <MaxBtn onClick={() => setMintAmount(MAX_VAL.toString())}>Max</MaxBtn>
-                </AmountInputWrapper>
-            </Stack>
-            <Stack spacing={1} marginTop={2}>
-                <Typography fontWeight={700} color="white">
-                    {disabled ? 'Currently Unavailable' : 'You have ' + reservedAmount + ' reserve mints'}
-                </Typography>
-                <Stack direction="row" alignItems="center" spacing={2}>
-                    <Stack direction="row" alignItems="center" spacing={1}>
-                        <MintBtn disabled={disabled} onClick={mint}>
-                            MINT
-                        </MintBtn>
-                        <ReserveBtn disabled={disabled} onClick={reserve}>
-                            RESERVE
-                        </ReserveBtn>
+                <Stack spacing={1} marginTop={3}>
+                    <Typography fontSize={14} fontWeight={400} color="white">
+                        # of Basketball Heads (Max 3)
+                    </Typography>
+                    <AmountInputWrapper sx={{ width: 184 }}>
+                        <AmountInputTextField value={mintAmount} onChange={handleInputChange} />
+                        <MaxBtn onClick={() => setMintAmount(MAX_VAL.toString())}>Max</MaxBtn>
+                    </AmountInputWrapper>
+                </Stack>
+                <Stack spacing={1} marginTop={2}>
+                    <Typography fontWeight={700} color="white">
+                        {disabled ? 'Currently Unavailable' : 'You have ' + reservedAmount + ' reserve mints'}
+                    </Typography>
+                    <Stack direction="row" alignItems="center" spacing={2}>
+                        <Stack direction="row" alignItems="center" spacing={1}>
+                            <MintBtn disabled={disabled} onClick={mint}>
+                                MINT
+                            </MintBtn>
+                            <ReserveBtn disabled={disabled} onClick={reserve}>
+                                RESERVE
+                            </ReserveBtn>
+                        </Stack>
                     </Stack>
                 </Stack>
             </Stack>
-        </Stack>
+            <Dialog
+                open={mintState === MintStatus.MINTING || reserveState === ReserveStatus.RESERVING}
+                maxWidth="lg"
+                PaperProps={{
+                    sx: {
+                        padding: 4,
+                        background: 'none',
+                    },
+                }}
+            >
+                <CircularProgress />
+            </Dialog>
+        </>
     );
 };
 
