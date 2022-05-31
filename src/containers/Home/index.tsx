bad9cf096dfb64851a3c0f5ce4fd4fb955fce53e
TrueLifeDev <truelife1228@outlook.com>
2022-05-31 13:47:46 -0500

    feat: updated video play on scroll in home page

--- src/containers/Home/index.tsx
+++ src/containers/Home/index.tsx
@@ -1,22 +1,43 @@
 import React, { useEffect, useRef } from 'react';
+import ReactPlayer from 'react-player';
 
 const HomePageContainer: React.FC = (): JSX.Element => {
-    const vidRef = useRef<HTMLVideoElement>(null);
+    const containerRef = useRef<HTMLDivElement>(null);
+    const playerRef = useRef<ReactPlayer>(null);
+
+    const [isReady, setIsReady] = React.useState(false);
+    // const [videoLen, setVideoLen] = React.useState<number>(0);
+    const [maxScrollY, setMaxScrollY] = React.useState<number>(0);
 
     useEffect(() => {
         const onScroll = (e: any) => {
-            vidRef.current?.play();
+            playerRef.current?.seekTo(window.scrollY / maxScrollY, 'fraction');
         };
         window.addEventListener('scroll', onScroll);
 
         return () => window.removeEventListener('scroll', onScroll);
     });
 
+    const onReady = React.useCallback(() => {
+        if (!isReady) {
+            // setVideoLen(playerRef.current ? playerRef.current.getDuration() : 0);
+            setMaxScrollY(document.documentElement.scrollHeight - document.documentElement.clientHeight);
+
+            setIsReady(true);
+        }
+    }, [isReady]);
+
     return (
-        <div>
-            <video ref={vidRef} muted loop style={{ width: '100%' }}>
-                <source src={'/assets/currycounter/background-mobile.mp4'} type="video/mp4" />
-            </video>
+        <div ref={containerRef}>
+            <ReactPlayer
+                ref={playerRef}
+                url="/assets/currycounter/background-mobile.mp4"
+                muted={false}
+                playing={false}
+                width="100%"
+                height="100%"
+                onReady={onReady}
+            />
         </div>
     );
 };
