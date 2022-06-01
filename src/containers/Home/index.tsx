e9c54c40c0f4baa5e6ed0de4df796bac95093eea
TrueLifeDev <truelife1228@outlook.com>
2022-06-01 02:23:34 -0500

    feat: updated video play frame by frame in home page

--- src/containers/Home/index.tsx
+++ src/containers/Home/index.tsx
@@ -1,44 +1,45 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import ReactPlayer from 'react-player';
+import style from './style';
 
 const HomePageContainer: React.FC = (): JSX.Element => {
-    const containerRef = useRef<HTMLDivElement>(null);
     const playerRef = useRef<ReactPlayer>(null);
 
-    const [isReady, setIsReady] = React.useState(false);
-    // const [videoLen, setVideoLen] = React.useState<number>(0);
-    const [maxScrollY, setMaxScrollY] = React.useState<number>(0);
+    const [seekPos, setSeekPos] = useState<number>(0);
+
+    const handleScroll = (event: any) => {
+        if (event.deltaY < 0) {
+            // console.log('scrolling up, seekPos:', seekPos);
+            setSeekPos(Math.max(seekPos - 0.003, 0));
+        } else if (event.deltaY > 0) {
+            // console.log('scrolling down, seekPos:', seekPos);
+            setSeekPos(Math.min(seekPos + 0.003, 1));
+        }
+    };
 
     useEffect(() => {
-        const onScroll = (e: any) => {
-            playerRef.current?.seekTo(window.scrollY / maxScrollY, 'fraction');
-        };
-        window.addEventListener('scroll', onScroll);
+        window.addEventListener('wheel', handleScroll);
 
-        return () => window.removeEventListener('scroll', onScroll);
-    });
+        return () => window.removeEventListener('wheel', handleScroll);
+    }, [seekPos]);
 
-    const onReady = React.useCallback(() => {
-        if (!isReady) {
-            // setVideoLen(playerRef.current ? playerRef.current.getDuration() : 0);
-            setMaxScrollY(document.documentElement.scrollHeight - document.documentElement.clientHeight);
-
-            setIsReady(true);
-        }
-    }, [isReady]);
+    useEffect(() => {
+        // console.log('seekPos:', seekPos);
+        playerRef.current?.seekTo(seekPos, 'fraction');
+    }, [seekPos]);
 
     return (
-        <div ref={containerRef}>
+        <>
             <ReactPlayer
                 ref={playerRef}
-                url="/assets/currycounter/background-mobile.mp4"
+                url="/assets/home/video.mp4"
                 muted={false}
                 playing={false}
                 width="100%"
-                height="100%"
-                onReady={onReady}
+                height="calc(100vh - 222px)"
             />
-        </div>
+            <style jsx>{style}</style>
+        </>
     );
 };
 
