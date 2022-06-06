c708a6b6d67427874bf7eb890a3016d827616d76
TrueLifeDev <truelife1228@outlook.com>
2022-06-06 14:09:18 -0500

    fix: roadmap page updated

--- src/containers/Roadmap/index.tsx
+++ src/containers/Roadmap/index.tsx
@@ -3,7 +3,6 @@ import { Stack, Box, Typography } from '@mui/material';
 import Container from '../Container';
 import RoadmapItemBox from '../../components/Roadmap/ItemBox';
 import Image from 'next/image';
-import BackgroundImg from '../../assets/roadmap/background.png';
 import { GradientBox } from './styles';
 import roadmapLists from '../../constants/roadmapData';
 
@@ -11,7 +10,7 @@ const RoadmapPageContainer: React.FC = (): JSX.Element => {
     return (
         <Box position="relative">
             <Stack position="absolute" top={0} left={0} right={0} zIndex={-10}>
-                <Image src={BackgroundImg} layout="responsive" alt="" />
+                <img src="/assets/roadmap/background.png" alt="" />
                 <GradientBox />
             </Stack>
             <Container sx={{ paddingY: 8 }}>
