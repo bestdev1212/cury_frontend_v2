b472375c9cb409a07ea8a320f12a7230a4b58236
TrueLifeDev <truelife1228@outlook.com>
2022-06-09 12:40:29 -0500

    feat: curry shop nf3 gcf info box component added

--- src/containers/CurryShop/index.tsx
+++ src/containers/CurryShop/index.tsx
@@ -12,7 +12,7 @@ import SerumGCFClaimBox from '../../components/CurryShop/Serums/GCFClaimBox';
 import SerumMintlistMintBox from '../../components/CurryShop/Serums/MintlistMintBox';
 import SerumGeneralMintBox from '../../components/CurryShop/Serums/GeneralMintBox';
 import { claimNF3GCF, claimSerumGCF, claimNF3CommunityNFT, claimSerumCommunityNFT } from '../../services/api/fetch';
-import StatusBox from '../../components/CurryShop/StatusBox';
+import NF3GCFInfoBox from '../../components/CurryShop/NF3InfoBox/GCFBox';
 import SerumStatusBox from '../../components/CurryShop/SerumStatusBox';
 import { ConnectMetamaskBtn, CategoryBtn } from './styles';
 import Image from 'next/image';
@@ -274,39 +274,7 @@ const CurryShopPageContainer: React.FC = (): JSX.Element => {
                             {selCategory === CategoryType.NF3_BASKETBALL && (
                                 <>
                                     {(!appState.jwtToken || dropPhase <= 1) && (
-                                        <Stack
-                                            direction="row"
-                                            padding={2}
-                                            spacing={2}
-                                            borderRadius={2}
-                                            sx={{
-                                                background:
-                                                    !!appState.jwtToken && dropPhase === 1 ? '#FFCA21' : '#1B1C22',
-                                            }}
-                                        >
-                                            <img
-                                                src="/assets/curryshop/gcf-claim.png"
-                                                width={80}
-                                                height={80}
-                                                alt=""
-                                                style={{ borderRadius: 8 }}
-                                            />
-                                            <Stack color={!!appState.jwtToken && dropPhase === 1 ? 'black' : 'white'}>
-                                                <Typography fontSize={20} fontWeight={800} marginTop={-1}>
-                                                    Genesis Curry Flow Freebies: NF3 Basketball
-                                                </Typography>
-                                                <Typography marginTop={2}>
-                                                    Snapshot Date:{' '}
-                                                    <span style={{ fontWeight: 700 }}>June 9, 2022 at 4 PM PST</span>
-                                                    <br />
-                                                    Start Date:{' '}
-                                                    <span style={{ fontWeight: 700 }}>June 9, 2022 at 5 PM PST</span>
-                                                    <br />
-                                                    End Date:{' '}
-                                                    <span style={{ fontWeight: 700 }}>June 11, 2022 at 5 PM PST</span>
-                                                </Typography>
-                                            </Stack>
-                                        </Stack>
+                                        <NF3GCFInfoBox jwtToken={appState.jwtToken} dropPhase={dropPhase} />
                                     )}
                                     {(!appState.jwtToken || dropPhase <= 2) && false && (
                                         <Stack
