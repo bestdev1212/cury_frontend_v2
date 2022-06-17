4d1f1112ea13da4c1f266a583351cf401587ac78
TrueLifeDev <truelife1228@outlook.com>
2022-06-17 13:17:21 -0500

    fix: updated menu order in nav bar

--- src/containers/Header/index.tsx
+++ src/containers/Header/index.tsx
@@ -21,15 +21,15 @@ type ComponentProps = {};
 const appMenuList = [
     // { title: 'Home', url: '/' },
 
-    { title: 'NF3 Counter', url: '/currycounter' },
-
     // { title: 'Curry Shop', url: '', comingSoon: { rightPos: -10 } },
     { title: 'Curry Shop', url: '/curryshop' },
 
-    { title: 'Mixology Room', url: '', comingSoon: { rightPos: -10 } },
-
     // { title: 'The Lab', url: '', comingSoon: { rightPos: -10 } },
     { title: 'The Lab', url: '/lab' },
+
+    { title: 'Mixology Room', url: '', comingSoon: { rightPos: -10 } },
+
+    { title: 'NF3 Counter', url: '/currycounter' },
 
     // { title: 'Roadmap', url: '', comingSoon: { rightPos: -10 } },
     { title: 'Roadmap', url: '/roadmap' },
