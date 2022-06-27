ccdc864d23b9c3cda677b9ed691246e56694136e
TrueLifeDev <truelife1228@outlook.com>
2022-06-27 17:11:31 -0500

    feat: how it works section updated in mixology page

--- src/containers/Mixology/NotWalletConnect/HowItWorks/index.tsx
+++ src/containers/Mixology/NotWalletConnect/HowItWorks/index.tsx
@@ -9,10 +9,77 @@ export interface ComponentProps {
 }
 
 const steps = [
-    { title: 'Select an NF3 Basketball', url: '/assets/mixology/howitworks/step1.mp4' },
-    { title: 'Choose up to 3 Serums', url: '/assets/mixology/howitworks/step1.mp4' },
-    { title: 'Fuse', url: '/assets/mixology/howitworks/step1.mp4' },
-    { title: 'Basketball Headz Avatar Created', url: '/assets/mixology/howitworks/step1.mp4' },
+    {
+        step_title: 'Select an NF3 Basketball',
+        url: '/assets/mixology/howitworks/step1.mp4',
+        isImg: false,
+        title: 'SELECT AN NF3 BASKETBALL',
+        desc: (
+            <Typography fontSize={20} fontWeight={600} lineHeight={1.2}>
+                The first step in creating your Basketball Headz avatar is selecting your NF3 Basketball. The NF3
+                Basketball does not hold any community traits, but it can still produce an avatar without any serums.
+                <br />
+                <br />
+                <br />
+                If you do not own an NF3 Basketball, you may purchase one through{' '}
+                <a href="https://opensea.io/" target="_blank" style={{ color: '#FFCA21' }}>
+                    Opensea
+                </a>
+                .
+            </Typography>
+        ),
+    },
+    {
+        step_title: 'Choose up to 3 Serums',
+        url: '/assets/mixology/howitworks/step2.gif',
+        isImg: true,
+        title: 'SELECT UP TO 3 SERUMS',
+        desc: (
+            <Typography fontSize={20} fontWeight={600} lineHeight={1.2}>
+                If you own Community Serums, select up to 3 types of your choice (the order in which Serums are selected
+                does not affect the final outcome). Each Serum guarantees one trait, with a possibility of two.
+                <br />
+                <br />
+                If you do not own any Serums, you can continue to the next step. Serums are not required. But if you
+                want to customize your Avatar, you can purchase the Serums through{' '}
+                <a href="https://opensea.io/" target="_blank" style={{ color: '#FFCA21' }}>
+                    Opensea
+                </a>
+                .
+            </Typography>
+        ),
+    },
+    {
+        step_title: 'Fuse',
+        url: '/assets/mixology/howitworks/step3.png',
+        isImg: true,
+        title: 'FUSE',
+        desc: (
+            <Typography fontSize={20} fontWeight={600} lineHeight={1.2}>
+                In the final step, we display all selected NFTs. Please make sure you have chosen the correct ones.
+                Fusing is an irreversible action, and the action will burn your selected NFTs, so make sure to
+                double-check.
+                <br />
+                <br />
+                When you are ready, click Fuse.
+            </Typography>
+        ),
+    },
+    {
+        step_title: 'Basketball Headz Avatar Created',
+        url: '/assets/mixology/howitworks/step4.gif',
+        isImg: true,
+        title: 'BASKETBALL HEADZ AVATAR CREATED',
+        desc: (
+            <Typography fontSize={20} fontWeight={600} lineHeight={1.2}>
+                You have created a Basketball Headz Avatar!
+                <br />
+                <br />
+                You will not see your Basketball Headz Avatar immediately. Please wait up to 24 hours for the metadata
+                to load.
+            </Typography>
+        ),
+    },
 ];
 
 const HowItWorks: React.FC<ComponentProps> = ({ sx }): JSX.Element => {
@@ -80,7 +147,7 @@ const HowItWorks: React.FC<ComponentProps> = ({ sx }): JSX.Element => {
                                         color={curStep === index ? 'white' : '#979797'}
                                         whiteSpace="nowrap"
                                     >
-                                        {step.title}
+                                        {step.step_title}
                                     </Typography>
                                 </Stack>
                                 <Box
@@ -101,11 +168,26 @@ const HowItWorks: React.FC<ComponentProps> = ({ sx }): JSX.Element => {
                         borderRadius={2}
                         marginTop={8}
                     >
-                        <Box width={224} height={224}>
-                            <video autoPlay muted loop style={{ width: '100%' }}>
-                                <source src={steps[curStep].url} type="video/mp4" />
-                            </video>
+                        <Box width={224} minWidth={224} height={224} borderRadius={2} overflow="hidden">
+                            {steps[curStep].isImg ? (
+                                <img src={steps[curStep].url} width="100%" />
+                            ) : (
+                                <video autoPlay muted loop style={{ width: '100%' }}>
+                                    <source src={steps[curStep].url} type="video/mp4" />
+                                </video>
+                            )}
                         </Box>
+                        <Stack spacing={2}>
+                            <Typography
+                                fontSize={{ xs: 32, md: 48 }}
+                                fontWeight={800}
+                                lineHeight={1}
+                                className="neueplak_condensed"
+                            >
+                                {steps[curStep].title}
+                            </Typography>
+                            {steps[curStep].desc}
+                        </Stack>
                     </Stack>
                 </Stack>
             </Container>
