9c1054f5772b782cdbab16a51b2cbd85eb3ffdf4
TrueLifeDev <truelife1228@outlook.com>
2022-06-27 14:30:53 -0500

    feat: how it works section updated in mixology page

--- src/containers/Mixology/NotWalletConnect/HowItWorks/index.tsx
+++ src/containers/Mixology/NotWalletConnect/HowItWorks/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Stack, Box, Typography } from '@mui/material';
 import { GradientBox } from './styles';
 import { SxProps } from '@mui/system';
@@ -8,7 +8,16 @@ export interface ComponentProps {
     sx?: SxProps;
 }
 
+const steps = [
+    { title: 'Select an NF3 Basketball' },
+    { title: 'Choose up to 3 Serums' },
+    { title: 'Fuse' },
+    { title: 'Basketball Headz Avatar Created' },
+];
+
 const HowItWorks: React.FC<ComponentProps> = ({ sx }): JSX.Element => {
+    const [curStep, setCurStep] = useState<number>(0);
+
     return (
         <Stack position="relative">
             <Box width="100%" height={720}></Box>
@@ -16,7 +25,84 @@ const HowItWorks: React.FC<ComponentProps> = ({ sx }): JSX.Element => {
                 <img src="/assets/mixology/background1.png" width="100%" />
                 <GradientBox />
             </Stack>
-            <Container sx={{ position: 'absolute', inset: 0 }}>sdfafasdf</Container>
+            <Container sx={{ position: 'absolute', inset: 0, paddingY: 16 }}>
+                <Stack alignItems="center">
+                    <Typography
+                        fontSize={{ xs: 32, md: 48 }}
+                        fontWeight={800}
+                        lineHeight={1}
+                        className="neueplak_condensed"
+                    >
+                        CREATING A BASKETBALL HEADZ AVATAR
+                    </Typography>
+                    <Typography
+                        fontSize={{ xs: 72, md: 128 }}
+                        fontWeight={800}
+                        lineHeight={1}
+                        sx={{ textDecoration: 'underline', textDecorationThickness: 'from-font' }}
+                        className="neueplak_condensed"
+                    >
+                        HOW IT WORKS
+                    </Typography>
+                    <Typography fontSize={20} fontWeight={600} lineHeight={1} marginTop={6}>
+                        Combine your NF3 Basketball with up to 3 Serums to determine how your avatar is made
+                    </Typography>
+                    <Stack width="80%" direction="row" alignItems="center" spacing={2} marginTop={8}>
+                        {steps.map((step, index) => (
+                            <>
+                                <Stack
+                                    alignItems="center"
+                                    spacing={2}
+                                    onClick={() => setCurStep(index)}
+                                    sx={{ cursor: 'pointer' }}
+                                    key={`step_key${index}`}
+                                >
+                                    <Stack
+                                        width={28}
+                                        height={28}
+                                        justifyContent="center"
+                                        alignItems="center"
+                                        borderRadius="100%"
+                                        sx={{ background: curStep === index ? '#FFCA21' : '#979797' }}
+                                    >
+                                        <Typography
+                                            fontSize={12}
+                                            fontWeight={600}
+                                            color={curStep === index ? 'black' : 'white'}
+                                            marginBottom="2px"
+                                        >
+                                            {index + 1}
+                                        </Typography>
+                                    </Stack>
+                                    <Typography
+                                        fontSize={14}
+                                        fontWeight={500}
+                                        color={curStep === index ? 'white' : '#979797'}
+                                        whiteSpace="nowrap"
+                                    >
+                                        {step.title}
+                                    </Typography>
+                                </Stack>
+                                <Box
+                                    flexGrow={1}
+                                    height="1px"
+                                    display={index === steps.length - 1 ? 'none' : 'block'}
+                                    sx={{ background: '#BDBDBD' }}
+                                />
+                            </>
+                        ))}
+                    </Stack>
+                    <Stack
+                        width="80%"
+                        direction="row"
+                        spacing={6}
+                        padding={4}
+                        border="1px solid #BDBDBD"
+                        borderRadius={2}
+                        marginTop={8}
+                    ></Stack>
+                </Stack>
+            </Container>
         </Stack>
     );
 };
