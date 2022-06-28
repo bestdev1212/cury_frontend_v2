d47fd07e8d0e404ffa3a2e7a3817330e7f39cf90
TrueLifeDev <truelife1228@outlook.com>
2022-06-28 13:14:14 -0500

    fix: roadmap updated

--- src/constants/roadmapData.tsx
+++ src/constants/roadmapData.tsx
@@ -2,8 +2,8 @@ import { Stack, Typography } from '@mui/material';
 
 const roadmapLists = [
     {
-        img: '/assets/roadmap/01.png',
-        title: '01/ GENESIS CURRY FLOW',
+        img: '/assets/roadmap/gcf.png',
+        title: 'GENESIS CURRY FLOW',
         desc: (
             <Typography>
                 How do you celebrate the moment that Stephen Curry broke the world record for three-point shooting,
@@ -14,94 +14,88 @@ const roadmapLists = [
         ),
     },
     {
-        img: '/assets/roadmap/02.png',
-        title: '02/ NFT MEETS NF3',
+        img: '/assets/roadmap/nft-meets-nf3.png',
+        title: 'Q2 2022: NFT MEETS NF3',
         desc: (
             <Typography>
-                As Curry continues to raise the bar through the 2022 NBA Playoffs, we launch our first ever NF3
+                As Stephen continues to raise the bar through the 2022 NBA Playoffs, we launch the amazing NF3
                 Basketball. Nine free NFT basketball minted every time he scores a 3 pointer. Yes, they are limited, and
-                that's the point; just like Curry, you have to have the quickest hands in the game.
+                that's the point; just like Steph, you have to have the quickest hands in the game.
             </Typography>
         ),
     },
     {
-        img: '/assets/roadmap/03.png',
-        title: `03/ IT'S ALL ABOUT COMMUNITY`,
+        img: '/assets/roadmap/all-about-community.png',
+        title: `Q2 2022: IT'S ALL ABOUT COMMUNITY`,
         desc: (
             <Typography>
                 We are excited to partner with some of the most amazing NFT projects in the Metaverse to grow one big
-                passionate basketball community. So prepare to get involved in the biggest cross-community project the
-                Metaverse has seen.
+                passionate basketball community. So prepare to be excited and get involved in the biggest
+                cross-community project the Metaverse has seen.
             </Typography>
         ),
     },
     {
-        img: '/assets/roadmap/04.png',
-        title: '04/ YOU DESERVE THE MINTLIST',
+        img: '/assets/roadmap/deserve-mintlist.png',
+        title: 'Q2 2022: YOU DESERVE THE MINTLIST',
         desc: (
-            <Stack>
-                <Typography>
-                    To reward our loyal holders of the Genesis Curry Flow, holders will be able to claim a free NF3
-                    Basketball and a free Serum based on the rarity of your Genesis Curry Flow!
-                </Typography>
-                <Typography lineHeight={1.2} marginTop={3}>
-                    <span style={{ fontWeight: 700 }}>Genesis Curry Flow Freebies for NF3 Basketballs:</span>
-                    <br />
-                    <br />
-                    Snapshot Date: <span style={{ color: '#FFCA21' }}>June 9th at 4:00:00 PM PST</span>
-                    <br />
-                    Start Date: <span style={{ color: '#FFCA21' }}>June 9th at 5:00:00 PM PST</span>
-                    <br />
-                    End Date: <span style={{ color: '#FFCA21' }}>June 11th at 5:00:00 PM PST</span>
-                    <br />
-                    <br />
-                    <span style={{ fontWeight: 700 }}>
-                        Early Purchase (Mintlist) for NF3 Basketballs and Serums, and Genesis Curry Flow Freebies for
-                        Serums:
-                    </span>
-                    <br />
-                    <br />
-                    Start Date: <span style={{ color: '#FFCA21' }}>June 19th at 5:00:00 PM PST</span>
-                    <br />
-                    Mintlist End Date: <span style={{ color: '#FFCA21' }}>June 21st at 5:00:00 PM PST</span>
-                </Typography>
-            </Stack>
+            <Typography>
+                To reward our loyal holders of the Genesis Curry Flow, as well as the Metaverse shoe holders (Sandbox,
+                Decentraland, RKL, Gala Games X Town Star), holders will be able to claim a free NF3 Basketball and a
+                free Serum based on the rarity of your Genesis Curry Flow! A snapshot for claiming will be taken on June
+                3rd and opened for claiming on June 3rd.
+            </Typography>
         ),
     },
     {
-        img: '/assets/roadmap/05.png',
-        title: '05/ MUTATE THE GAME',
+        img: '/assets/roadmap/mutate-game.png',
+        title: 'Q2 2022: MUTATE THE GAME',
         desc: (
             <Typography>
                 Bringing together the NF3 and traits from our community partners, serums from each partner will be
-                available for purchase from our store. Mix them together in our high-tech lab and prepare to unlock your
-                unique and generative Basketball Headz avatar.
+                available for purchase from our store. So buy up to three and mix them together in our high-tech lab to
+                create your very own unique and generative 'Basketball Head' avatar.
                 <br />
                 <br />
-                <b>General Purchase for NF3 Basketballs and Serums:</b>
-                <br />
-                <br />
-                NF3 Basketball Start Date: <span style={{ color: '#FFCA21' }}>June 29th at 5:00:00 PM PST</span>
-                <br />
-                Serum Start Date: <span style={{ color: '#FFCA21' }}>June 29th at 5:00:00 PM PST</span>
+                Serums and NF3 Basketballs open on DATE_TIME_FOR_SHOP_OPEN.
             </Typography>
         ),
     },
     {
-        img: '/assets/roadmap/06.png',
-        title: `06/ WHAT'S NEXT`,
+        img: '/assets/roadmap/metaverse-partner-goods.png',
+        title: 'Q3 2022: METAVERSE PARTNER GOODS',
         desc: (
             <Typography>
-                Curry Brand is committed to building the most vibrant basketball community in the Metaverse. So stay
-                tuned as we continue to innovate with new drops to delight and reward our fans.
+                Join the largest, most positive basketball community in the Metaverse.
+                <br />
+                Holders will be the first to access Metaverse partner's goods collaborations with Curry Brand - some of
+                these you'll see are created as part of the drop!
             </Typography>
         ),
     },
-    // {
-    //     img: '/assets/roadmap/curry.png',
-    //     title: `07/ WHAT'S NEXT`,
-    //     desc: 'State-of-the-art 3D apparel locker, interoperable avatars, 3D file usage, real-life merchandise in the likeness of your avatar? Coming soon…',
-    // },
+    {
+        img: '/assets/roadmap/plug-and-play-digital-wearables.png',
+        title: 'Q4 2022: PLUG & PLAY DIGITAL WEARABLES',
+        desc: (
+            <Typography>
+                Avatars and goods are built as detachable 3D assets. Plug and play digital wearables and goods on your
+                avatar
+                <br />
+                Aside from holding a 3D avatar PFP, holders of the Basketball Head NFT will get exclusive or early
+                access to future Curry Brand goods.
+            </Typography>
+        ),
+    },
+    {
+        img: '/assets/roadmap/interoperable-avatars.png',
+        title: 'Q1 2023: INTEROPERABLE AVATARS',
+        desc: (
+            <Typography>
+                Interoperable avatars, IRL events/programming, and more! Let us know what you want us to create. Open to
+                feedback on #suggestions in Discord.
+            </Typography>
+        ),
+    },
 ];
 
 export default roadmapLists;
