6e19fdc44520a7b7a4a05e1c879a3686454e791a
TrueLifeDev <truelife1228@outlook.com>
2022-06-28 13:32:47 -0500

    fix: roadmap updated

--- src/constants/roadmapData.tsx
+++ src/constants/roadmapData.tsx
@@ -1,52 +1,52 @@
 import { Stack, Typography } from '@mui/material';
 
 const roadmapLists = [
-    {
-        img: '/assets/roadmap/gcf.png',
-        title: 'GENESIS CURRY FLOW',
-        desc: (
-            <Typography>
-                How do you celebrate the moment that Stephen Curry broke the world record for three-point shooting,
-                changing the game for good? You change the Metaverse for good with the Curry Genesis Flow, the first
-                wearable to cross the platforms of Decentraland, Sandbox, Gala Games, and RKL. Minted in five distinct
-                rarities and now delivering the NF3 Basketball to our loyal holders.
-            </Typography>
-        ),
-    },
-    {
-        img: '/assets/roadmap/nft-meets-nf3.png',
-        title: 'Q2 2022: NFT MEETS NF3',
-        desc: (
-            <Typography>
-                As Stephen continues to raise the bar through the 2022 NBA Playoffs, we launch the amazing NF3
-                Basketball. Nine free NFT basketball minted every time he scores a 3 pointer. Yes, they are limited, and
-                that's the point; just like Steph, you have to have the quickest hands in the game.
-            </Typography>
-        ),
-    },
-    {
-        img: '/assets/roadmap/all-about-community.png',
-        title: `Q2 2022: IT'S ALL ABOUT COMMUNITY`,
-        desc: (
-            <Typography>
-                We are excited to partner with some of the most amazing NFT projects in the Metaverse to grow one big
-                passionate basketball community. So prepare to be excited and get involved in the biggest
-                cross-community project the Metaverse has seen.
-            </Typography>
-        ),
-    },
-    {
-        img: '/assets/roadmap/deserve-mintlist.png',
-        title: 'Q2 2022: YOU DESERVE THE MINTLIST',
-        desc: (
-            <Typography>
-                To reward our loyal holders of the Genesis Curry Flow, as well as the Metaverse shoe holders (Sandbox,
-                Decentraland, RKL, Gala Games X Town Star), holders will be able to claim a free NF3 Basketball and a
-                free Serum based on the rarity of your Genesis Curry Flow! A snapshot for claiming will be taken on June
-                3rd and opened for claiming on June 3rd.
-            </Typography>
-        ),
-    },
+    // {
+    //     img: '/assets/roadmap/gcf.png',
+    //     title: 'GENESIS CURRY FLOW',
+    //     desc: (
+    //         <Typography>
+    //             How do you celebrate the moment that Stephen Curry broke the world record for three-point shooting,
+    //             changing the game for good? You change the Metaverse for good with the Curry Genesis Flow, the first
+    //             wearable to cross the platforms of Decentraland, Sandbox, Gala Games, and RKL. Minted in five distinct
+    //             rarities and now delivering the NF3 Basketball to our loyal holders.
+    //         </Typography>
+    //     ),
+    // },
+    // {
+    //     img: '/assets/roadmap/nft-meets-nf3.png',
+    //     title: 'Q2 2022: NFT MEETS NF3',
+    //     desc: (
+    //         <Typography>
+    //             As Stephen continues to raise the bar through the 2022 NBA Playoffs, we launch the amazing NF3
+    //             Basketball. Nine free NFT basketball minted every time he scores a 3 pointer. Yes, they are limited, and
+    //             that's the point; just like Steph, you have to have the quickest hands in the game.
+    //         </Typography>
+    //     ),
+    // },
+    // {
+    //     img: '/assets/roadmap/all-about-community.png',
+    //     title: `Q2 2022: IT'S ALL ABOUT COMMUNITY`,
+    //     desc: (
+    //         <Typography>
+    //             We are excited to partner with some of the most amazing NFT projects in the Metaverse to grow one big
+    //             passionate basketball community. So prepare to be excited and get involved in the biggest
+    //             cross-community project the Metaverse has seen.
+    //         </Typography>
+    //     ),
+    // },
+    // {
+    //     img: '/assets/roadmap/deserve-mintlist.png',
+    //     title: 'Q2 2022: YOU DESERVE THE MINTLIST',
+    //     desc: (
+    //         <Typography>
+    //             To reward our loyal holders of the Genesis Curry Flow, as well as the Metaverse shoe holders (Sandbox,
+    //             Decentraland, RKL, Gala Games X Town Star), holders will be able to claim a free NF3 Basketball and a
+    //             free Serum based on the rarity of your Genesis Curry Flow! A snapshot for claiming will be taken on June
+    //             3rd and opened for claiming on June 3rd.
+    //         </Typography>
+    //     ),
+    // },
     {
         img: '/assets/roadmap/mutate-game.png',
         title: 'Q2 2022: MUTATE THE GAME',
@@ -93,6 +93,18 @@ const roadmapLists = [
             <Typography>
                 Interoperable avatars, IRL events/programming, and more! Let us know what you want us to create. Open to
                 feedback on #suggestions in Discord.
+            </Typography>
+        ),
+    },
+    {
+        img: '/assets/roadmap/physical-2974-shoes.png',
+        title: 'Q1 2023: PHYSICAL 2974 SHOES',
+        desc: (
+            <Typography>
+                Raffled off to holders of Genesis Curry Flow and Basketball Headz collectors.
+                <br />
+                <br />
+                Quantity TBD
             </Typography>
         ),
     },
