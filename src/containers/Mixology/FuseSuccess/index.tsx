437d56c2e3a11d99b00a0de869caed79c6887e27
TrueLifeDev <truelife1228@outlook.com>
2022-06-30 14:14:01 -0500

    feat: confetti effect added in fuse success page

--- src/containers/Mixology/FuseSuccess/index.tsx
+++ src/containers/Mixology/FuseSuccess/index.tsx
@@ -4,9 +4,13 @@ import { useAppContext } from '../../../context/AppContext';
 import { MutantImgBox, GotoLabBtn, BackToMixRoom } from './styles';
 import Image from 'next/image';
 import Link from 'next/link';
+import Confetti from 'react-confetti';
+import useWindowSize from 'react-use/lib/useWindowSize';
 
 const FuseSuccess: React.FC = (): JSX.Element => {
     const [appState, setAppState] = useAppContext();
+
+    const { width, height } = useWindowSize();
 
     const onBackToMixologyRoom = () => {
         setAppState({
@@ -19,25 +23,30 @@ const FuseSuccess: React.FC = (): JSX.Element => {
     };
 
     return (
-        <Stack alignItems="center" spacing={6} marginX={4}>
-            <Typography fontSize={48} fontWeight={700} lineHeight={1} textAlign="center">
-                You have successfully minted your
-                <br />
-                <span style={{ color: '#FFCA21' }}>Basketball Headz Avatar</span>
-            </Typography>
-            {/* <MutantImgBox marginTop={6}>
+        <>
+            <Stack alignItems="center" spacing={6} marginX={4}>
+                <Typography fontSize={48} fontWeight={700} lineHeight={1} textAlign="center">
+                    You have successfully minted your
+                    <br />
+                    <span style={{ color: '#FFCA21' }}>Basketball Headz Avatar</span>
+                </Typography>
+                {/* <MutantImgBox marginTop={6}>
                 <Image src="/assets/nft-items/mutant.png" width={320} height={320} alt="" className="mutant_img" />
             </MutantImgBox> */}
-            <Typography lineHeight={1}>Please wait up to 24 hrs for your Basketball Headz Avatar reveal!</Typography>
-            <Stack direction="row" spacing={2}>
-                <Link href="/lab" passHref>
-                    <a rel="noopener noreferrer">
-                        <GotoLabBtn>Go to The Lab</GotoLabBtn>
-                    </a>
-                </Link>
-                <BackToMixRoom onClick={onBackToMixologyRoom}>Back to the Mixology Room</BackToMixRoom>
+                <Typography lineHeight={1}>
+                    Please wait up to 24 hrs for your Basketball Headz Avatar reveal!
+                </Typography>
+                <Stack direction="row" spacing={2}>
+                    <Link href="/lab" passHref>
+                        <a rel="noopener noreferrer">
+                            <GotoLabBtn>Go to The Lab</GotoLabBtn>
+                        </a>
+                    </Link>
+                    <BackToMixRoom onClick={onBackToMixologyRoom}>Back to the Mixology Room</BackToMixRoom>
+                </Stack>
             </Stack>
-        </Stack>
+            <Confetti width={width - 32} height={height - 100} />
+        </>
     );
 };
 
