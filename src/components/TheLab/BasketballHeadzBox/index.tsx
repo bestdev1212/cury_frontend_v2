02fc5862d280258454866d24570a4a9f11e7519e
TrueLifeDev <truelife1228@outlook.com>
2022-07-06 12:49:30 -0500

    fix: basketballheadz file(aws file)download test code added

--- src/components/TheLab/BasketballHeadzBox/index.tsx
+++ src/components/TheLab/BasketballHeadzBox/index.tsx
@@ -18,20 +18,26 @@ const BasketballHeadzBox: React.FC<ComponentProps> = ({
     selectedBasketballHeadzTokenId,
     setSelectedBasketballHeadzTokenId,
 }): JSX.Element => {
-    // const forceDownload = (url: string) => {
-    //     let urlArray = url.split('/');
-    //     let bucket = urlArray[3];
-    //     let key = `${urlArray[4]}/${urlArray[5]}`;
-    //     let s3 = new AWS.S3({ params: { Bucket: bucket } });
-    //     let params = { Bucket: bucket, Key: key };
-    //     s3.getObject(params, (err, data) => {
-    //         let blob = new Blob([data.Body], { type: data.ContentType });
-    //         let link = document.createElement('a');
-    //         link.href = window.URL.createObjectURL(blob);
-    //         link.download = url;
-    //         link.click();
-    //     });
-    // };
+    const handleDownload = (url: string) => {
+        var AWS = require('aws-sdk');
+        AWS.config.update({
+            accessKeyId: 'AKIATRYTS26Z4ZJK5DFK',
+            secretAccessKey: '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',
+            region: 'us-east-2',
+        });
+        let fileName = url.split('/').pop();
+
+        var s3 = new AWS.S3({ params: { Bucket: 'luna-bucket' } });
+        s3.getObject({ Bucket: 'luna-bucket', Key: `3d-avatar-dev/${fileName}` }, function (error: any, data: any) {
+            if (error != null) {
+                console.log('Failed to retrieve an object: ' + error);
+            } else {
+                console.log('Loaded ' + data.ContentLength + ' bytes');
+                // do something with data.Body
+            }
+        });
+    };
+
     const onClickItem = () => {
         if (selectedBasketballHeadzTokenId === item.tokenId) {
             setSelectedBasketballHeadzTokenId('');
@@ -77,7 +83,7 @@ const BasketballHeadzBox: React.FC<ComponentProps> = ({
                         </a>
                     </Link>
                     {/* <a href="" download={item.image}> */}
-                    <MenuBtn /* onClick={() => forceDownload(item.image)} */>
+                    <MenuBtn onClick={() => handleDownload(item.image)}>
                         <Icon icon="ic:outline-file-download" fontSize={24} />
                         <Typography fontSize={14} fontWeight={600} marginLeft={1} padding="0 0 4px">
                             Download
